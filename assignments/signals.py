a5fb8ad276b2e33d7a496e88afc558f167a25fc3
Jeffkungu <Jeffkungu@gmail.com>
2017-09-19 11:49:02 +0300

    [Feature] remeove receiver-signal connection from the file

--- assignments/signals.py
+++ assignments/signals.py
@@ -4,5 +4,3 @@ from .models import Assignment
 def assignment_callback(sender, created, **kwargs):
     if created:
         print "Assignment Created!"
-
-post_save.connect(assignment_callback, sender=Assignment, dispatch_uid="assignment_callback")
