5f7413c0f9d2557bb8a0c14b81c38953fc3d7f2e
Jeffkungu <Jeffkungu@gmail.com>
2017-09-19 11:44:31 +0300

    [Feature] adds post_save signal to the ready() method

--- assignments/apps.py
+++ assignments/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_save
+from .signals import assignment_callback
+from .models import Assignment
 
 
 class AssignmentsConfig(AppConfig):
     name = 'assignments'
+
+    def ready(self):
+        post_save.connect(assignment_callback, sender=Assignment, dispatch_uid="assignment_callback")
